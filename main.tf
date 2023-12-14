9c6b2036b19104f41620c328c15acfac50b3b5cc
Pratikraut@12345 <pratikraut88895@gmail.com>
2023-12-14 21:38:02 +0530

    add  terraorm

--- main.tf
+++ main.tf
@@ -13,12 +13,15 @@ provider "render" {
 }
 
 resource "render_service" "reactjs" {
-  name = "admin-panel"
-  repo = "https://github.com/pratikdevelop/admin-panel"
-  type = "static_site"
+  name   = "admin-panel"
+  repo   = "https://github.com/pratikdevelop/admin-panel"
+  branch = "master"
+  type   = "static_site"
 
   static_site_details = {
-    build_command = "npm run build;"
+    build_command = "npm install --force && npm run build;"
     publish_path  = "dist"
+    force_https   = true
+
   }
 }
